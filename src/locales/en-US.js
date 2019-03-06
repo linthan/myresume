1c5342c5b5cc48da7c55fa79f1b93889a82436f6
fusikai <fusikai@douyu.tv>
2019-03-06 14:40:33 +0800

    tmp save

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -1,4 +1,16 @@
 export default {
   download: "Download",
-  switchTo: "切换成中文"
+  switchTo: "切换成中文",
+  lastModified: "Updated in March 6, 2019",
+  name: "Ethan",
+  job: "Backend Development Engineer & Full Stack Engineer",
+  gender: "Male",
+  masterSchool: " Huazhong University of Science and Technology ",
+  masterMajor: " Computer Technology ",
+  postgraduate: "Postgraduate",
+  graduatedAt: "Graduated In 2017",
+  blog: "Technical Blog",
+  github: "Github: linthan",
+  email: "Email: 445366464@qq.com",
+  phone: "Phone: 18971149078 "
 };
--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -1,4 +1,16 @@
 export default {
   download: "下载",
-  switchTo: "Switch to English"
+  switchTo: "Switch to English",
+  lastModified: "最后更新于2019年3月6日",
+  name: "付四凯",
+  job: "Web后端开发工程师 | 全栈工程师",
+  gender: "男",
+  masterSchool: " 华中科技大学 ",
+  masterMajor: " 计算机技术 ",
+  postgraduate: "研究生",
+  graduatedAt: "2017年毕业",
+  blog: "技术博客",
+  github: "Github: linthan",
+  email: "邮箱: 445366464@qq.com",
+  phone: "电话: 18971149078"
 };
